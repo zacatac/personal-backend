56c66b4ad90ae4f5e0c256e5fe97aa55dc59e315
Zack Field <zack@complete.so>
2024-07-23 23:20:38 -0700

    parameterize db url

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
-)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+assert SQLALCHEMY_DATABASE_URL is not None, "DATABASE_URL env var not set"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
