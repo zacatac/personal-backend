d9b8d72863cca82ee98555b23a3326f23835c678
Zack Field <zack@complete.so>
2024-07-23 22:18:49 -0700

    wire up dotenv

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from dotenv import load_dotenv
 
 from app.database import Base
 from app.models import User, Bot
+
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
