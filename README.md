6f39e6e08c32b2b9a25f3d4c64cfec48f383cce6
Zack Field <field.zackery@gmail.com>
2024-07-30 15:37:05 -0700

    cleanup readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # personal-backend
+
+The FastAPI backend for my personal website, served at api.zackeryfield.com. Right now this just has an exploration of a streaming chat usecase called [onebot](https://zackeryfield.com/onebot).
+
+Most of the content on my personal site is a SPA served from this [repo](https://github.com/zacatac/personal). Why two repos? It's just easier to set up deploy triggers this way with Railway 😅.
--- alembic/env.py
+++ alembic/env.py
@@ -12,25 +12,12 @@ from app.models import User, Bot
 
 load_dotenv()
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 database_url = os.getenv("DATABASE_URL")
 assert database_url is not None, "DATABASE_URL env var not set"
